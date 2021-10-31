988acee3658c98c9ef03a3d38171a35d7becdf9e
umaryusuf <umaryusuf602@gmail.com>
2021-10-31 15:50:29 +0100

    added other buttons implementations and actions listeners

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,25 +6,55 @@ import javax.swing.*;
 public class Calculator extends JFrame implements ActionListener {
 	private JPanel panel;
 	private JTextArea output;
-	private JButton addBtn, subtractBtn, devideBtn, timesBtn, equalBtn, dotbtn;
+	private JButton addBtn, subtractBtn, devideBtn, timesBtn, equalBtn, dotBtn;
 	private JButton btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0;
 	
 	public Calculator() {
 		panel = new JPanel();
-		
+		//panel.setLayout(new GridBagLayout());
 		output = new JTextArea(3, 22);
 		
 		addBtn = new JButton("+");
 		subtractBtn = new JButton("-");
 		devideBtn = new JButton("x");
 		timesBtn = new JButton("/");
+		equalBtn = new JButton("=");
+		dotBtn = new JButton(".");
 		
+		btn1 = new JButton("1");
+		btn2 = new JButton("2");
+		btn3 = new JButton("3");
+		btn4 = new JButton("4");
+		btn5 = new JButton("5");
+		btn6 = new JButton("6");
+		btn7 = new JButton("7");
+		btn8 = new JButton("8");
+		btn9 = new JButton("9");
+		btn0 = new JButton("0");
+
 		addBtn.addActionListener(this);
 		subtractBtn.addActionListener(this);
 		devideBtn.addActionListener(this);
 		timesBtn.addActionListener(this);
+		equalBtn.addActionListener(this);
+		dotBtn.addActionListener(this);
+		
+		btn1.addActionListener(this);
+		btn2.addActionListener(this);
+		btn3.addActionListener(this);
+		btn4.addActionListener(this);
+		btn5.addActionListener(this);
+		btn6.addActionListener(this);
+		btn7.addActionListener(this);
+		btn8.addActionListener(this);
+		btn9.addActionListener(this);
+		btn0.addActionListener(this);
 		
 		panel.add(output);
+		panel.add(btn1);
+		panel.add(btn2);
+		panel.add(btn3);
+		panel.add(addBtn);
 		
 		this.add(panel);
 		this.setTitle("Calculator");
