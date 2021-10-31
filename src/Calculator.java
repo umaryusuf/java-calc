fee6ad1dba969c87c5fe863bdf227f73aa354c2a
umaryusuf <umaryusuf602@gmail.com>
2021-10-31 15:06:21 +0100

    updated calc

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,54 @@
 
-public class Calculator {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-	public Calculator() {
-		// TODO Auto-generated constructor stub
+class Calculator extends JFrame implements ActionListener 
+{
+    JPanel panel,
+	JButton add,subtract,multiply,divide;
+    JTextArea output;
+    JLabel result,enter1,enter2;
+    
+    Calculator()
+    {
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
+        output = new JTextArea(13, 20);
+        
+        
+    }
+    
+        
+	public static void main(String args[])
+	{
+	    Calculator ob = new Calculator();
+	    ob.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    ob.setTitle("Calculator");
+	    ob.setVisible(true);
+	    ob.setSize(350,500);
 	}
 
-	public static void main(String[] args) {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	private void addItem(JPanel p, JComponent c, int x, int y, int width, int height, int align)
+	{
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.gridx = x;
+		gc.gridy = y;
+		gc.gridwidth = width;
+		gc.gridheight = height;
+		gc.weightx = 100.0;
+		gc.weighty = 100.0;
+		gc.insets = new Insets(5, 5, 5, 5);
+		gc.anchor = align;
+		gc.fill = GridBagConstraints.NONE;
+		p.add(c, gc);
+	}
 }
